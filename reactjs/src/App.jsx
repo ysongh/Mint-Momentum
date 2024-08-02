3c2654fc21637ddd8429d81e40941341132e4188
You Song Hou <ysongweb3@gmail.com>
2024-08-02 18:32:41 -0400

    feat: install and add Chakra UI

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import { ChakraProvider, Text } from '@chakra-ui/react';
 
 function App() {
 
   return (
-    <>
+    <ChakraProvider>
       <div>
        <h1>Mint Momentum</h1>
       </div>
-    </>
+    </ChakraProvider>
   )
 }
 
