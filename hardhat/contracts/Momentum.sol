8feffaf82dd0e613909262b5b7db540513fc2cb5
You Song Hou <ysongweb3@gmail.com>
2024-08-03 19:16:23 -0400

    feat: add a contract

---
+++ hardhat/contracts/Momentum.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+contract Momentum {
+    constructor(){}
+
+    MotivationalTip[] private motivationalTips;
+
+    struct MotivationalTip {
+        address owner;
+        string text;
+    }
+
+    function getMotivationalTips() public view returns (MotivationalTip[] memory){
+        return motivationalTips;
+    }
+
+    function createMotivationalTip(string memory _text) public {
+        motivationalTips.push(MotivationalTip(msg.sender, _text));
+    }
+}
