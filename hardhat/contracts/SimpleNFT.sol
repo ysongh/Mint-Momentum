1409ae2d423a57424ae786ff50e6d74261b8fcb0
You Song Hou <ysongweb3@gmail.com>
2024-08-03 16:57:37 -0400

    feat: add a simple NFT contract

---
+++ hardhat/contracts/SimpleNFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleNFT is ERC721 {
+    uint256 public nextTokenId;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function mint(address to) public returns (uint256) {
+        uint256 tokenId = nextTokenId++;
+        _mint(to, tokenId);
+        return tokenId;
+    }
+}
