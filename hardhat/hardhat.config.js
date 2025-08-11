51cee6770e8c1946434c5b3f9f2c1c8598f3d368
You Song Hou <ysongweb3@gmail.com>
2025-08-10 23:48:49 -0400

    chore: update hardhat.config.js to deploy contracts on Mantle Sepolia Testnet

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -9,6 +9,10 @@ module.exports = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATEKEY],
     },
+    "mantleSepolia": {
+      url: "https://rpc.sepolia.mantle.xyz", // Sepolia Testnet
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
   },
   paths: {
     artifacts: '../reactjs/src/artifacts',
